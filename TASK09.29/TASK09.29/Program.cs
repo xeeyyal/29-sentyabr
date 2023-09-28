3aa6a02137f51b0f499a980da2cd8fb709abf35f
xeeyyal <tu5luzvdh@code.edu.az>
2023-09-28 23:12:03 +0400

    task09.29 my commit

---
+++ TASK09.29/TASK09.29/Program.cs
@@ -0,0 +1,31 @@
+﻿//Task-1
+//int[] numbers = { 1, 4, 7, 8, 11 };
+
+//int sum = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//}
+
+//Console.WriteLine(sum);
+
+
+//Task-2
+//string[] words = { "baki", "gence", "serur", "eli", "bal" };
+
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i].Length > 4)
+//    {
+//        Console.WriteLine(words[i]);
+//    }
+//}
+
+//Task-3
+//int[] num = { 2, 5, 7, 9, 12, 13 };
+
+//for (int i = num.Length - 1; i >= 0; i--)
+//{
+//    Console.WriteLine(num[i]);
+//}
